1e9274d05e53ac155eeb35781d7dc7fa0389f226
Bjorn Blixhavn <bjorn@micronitor.com>
2016-02-17 13:37:18 +0100

    First commit for this repo.

---
+++ main.c
@@ -0,0 +1,18 @@
+/*
+ * git_training.c
+ *
+ * Created: 17.02.2016 13:35:01
+ * Author : Bjorn
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
